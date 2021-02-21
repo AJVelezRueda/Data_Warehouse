c5b5a2fc4346009d8fa8289cbabdc199bdb166bb
AJVelezRueda <anavelezrueda@gmail.com>
2021-02-21 12:22:26 -0300

    toggle in search mre

--- front-end/assets/js/search.js
+++ front-end/assets/js/search.js
@@ -1,6 +1,6 @@
 const moreOptionsIcon = document.getElementById("expand-options-img");
 const moreOptionsSections = document.getElementById("more-options");
 
-moreOptionsIcon.addEventListener("click", () => {
-    enableDomObject(moreOptionsSections);
+$(moreOptionsIcon).on("click", () => {
+   $(moreOptionsSections).toggleClass(["disable", "enable"]);
 })
